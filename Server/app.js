00e114f1a5429410d5df9d7481592d54338bf422
Dharmik Govani <63103191+21Shadow10@users.noreply.github.com>
2021-05-11 11:35:32 +0530

    Express Routing and MVC

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,7 @@
 const express = require('express') ;
 const morgan = require('morgan') ;
 const mongoose = require('mongoose') ;
-const Blog = require('./models/blog') ;
+const blogRoutes = require('./routes/blogRoutes') ;
 
 //express app
 const app = express() ;
@@ -86,51 +86,6 @@ app.get('/' , (req,res) => {
     res.redirect('/blogs') ;
 }) ;
 
-//Blogs Routes
-app.get('/blogs' ,(req,res) => {
-    Blog.find().sort( {createdAt: -1 })
-        .then((result) => {
-            res.render('index' , { title: 'All Blogs', blogs: result }) ;
-        })
-        .catch((err) => {
-            console.log(err) ;
-        })
-})
-
-app.post('/blogs' , (req,res) => {
-    const blog = new Blog(req.body) ;
-
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs')
-        })
-        .catch((err) => {
-            console.log(err) ;
-        })
-}) ;
-
-app.delete('/blogs/:id' , (req,res) => {
-    const id = req.params.id;
-
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirects: '/blogs' }) ;
-        })
-        .catch(err => console.log(err));
-})
-
-
-app.get('/blogs/:id' , (req,res) => {
-    const id = req.params.id ;
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', { title: 'Blog-details', blog: result }) ;
-        })
-        .catch((err) => {
-            console.log(err) ;
-        })
-})
-
 //About Page
 app.get('/about' , (req,res) => {
     // res.send('<p>About Page</p>') ;
@@ -143,9 +98,9 @@ app.get('/about-us' ,(req,res) =>{
     res.redirect('/about') ;
 })*/
 
-app.get('/blogs/create' , (req,res) => {
-    res.render('create', {title: 'New Blog'}) ;
-})
+
+//Blog Routes
+app.use('/blogs', blogRoutes) ;
 
 //404 page
 app.use((req,res) => {
